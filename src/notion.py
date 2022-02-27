2f3010b575b1cd4808f2e18d4e125eed522a5f65
Santiago <shpotes3@gmail.com>
2022-02-26 20:34:49 -0500

    WIP: add initial notion functionalities TODO: only update new events!

---
+++ src/notion.py
@@ -0,0 +1,42 @@
+from typing import Union
+import json
+from pathlib import Path
+
+from notion_client import Client
+
+from .structs import Event
+
+
+def get_service(secrets_dir: Union[Path, str]) -> Client:
+    """
+    Returns a Notion client object.
+    Args:
+        secrets_dir: Path to the directory containing the secrets.json file.
+    Returns:
+        A Notion client object.
+    """
+    secrets_dir = Path(secrets_dir)
+    # Get the notion token
+    with open(secrets_dir / 'notion_secrets.json', 'r') as buf:
+        notion_secrets = json.load(buf)
+        token = notion_secrets['token']
+    # Create the client
+    client = Client(auth=token)
+
+    return client
+
+
+def create_event(
+    service: Client,
+    database_id: str,
+    event: Event
+) -> dict:
+    """
+    Creates a new event in a given Notion database.
+    """
+    service.pages.create(
+        parent={
+            "database_id": database_id
+        },
+        **event.to_notion_page()
+    )
---
+++ src/structs.py
@@ -0,0 +1,134 @@
+from typing import Optional, Union, List
+from dataclasses import dataclass
+
+
+def notion_text(text: Optional[str]) -> dict:
+    """
+    Returns a Notion-friendly version of the text.
+    Args:
+        text: The text to be formatted.
+    Returns:
+        A Notion-friendly version of the text.
+    """
+    content = '' if text is None else text
+
+    return {
+        "rich_text": [{
+            "text": {
+                "content": content
+            }
+        }]
+    }
+
+
+def notion_url(url: Optional[str]) -> dict:
+    """
+    Returns a Notion-friendly version of the URL.
+    Args:
+        url: The URL to be formatted.
+    Returns:
+        A Notion-friendly version of the URL.
+    """
+    content = '' if url is None else url
+
+    return {
+        "url": content
+    }
+
+
+def notion_block(type: Union[str, List[str]], text_dict: dict) -> dict:
+    """
+    Returns a Notion-friendly version of a block.
+    Args:
+        type: The type of the block.
+        text_dict: The text to be formatted.
+    Returns:
+        A Notion-friendly version of the block.
+    """
+    if isinstance(text_dict["content"], str):
+        output_dict = {
+            "object": "block",
+            "type": type,
+            type: {
+                "text": [{
+                    "type": "text",
+                    "text": text_dict
+                }]
+            }
+        }
+    else:
+        output_dict = {
+            "object": "block",
+            "type": type,
+            type: {
+                "text": [
+                    {"type": "text", "text": text_chunk}
+                    for text_chunk in text_dict["content"]
+                ]
+            }
+        }
+
+    return output_dict
+
+
+@dataclass
+class Event:
+    start: str
+    end: str
+    title: str
+    location: Optional[str] = None
+    gcal_id: Optional[str] = None
+
+    def _get_notion_page_title(self) -> dict:
+        return {
+            "title": [
+                {
+                    "text": {
+                        "content": self.title
+                    }
+                }
+            ]
+        }
+
+    def _get_notion_date(self):
+        return {
+            "date": {
+                "start": self.start[:-6]+"Z",
+                "end": self.end[:-6]+"Z"
+            }
+        }
+
+    def to_notion_page(self) -> dict:
+        """
+        Returns a dict representing a Notion page.
+        """
+        properties = {}
+        properties["Event"] = self._get_notion_page_title()
+        properties["Date"] = self._get_notion_date()
+        properties["gcal_id"] = notion_text(self.gcal_id)
+
+        if self.location:
+            properties["Meeting"] = notion_url(self.location)
+
+        return {
+            "properties": properties,
+        }
+
+    @classmethod
+    def from_dict(cls, event: dict) -> 'Event':
+        start_date = event['start'].get('dateTime', event['start'].get('date'))
+        end_date = event['end'].get('dateTime', event['end'].get('date'))
+        if 'hangoutLink' in event:
+            location = event['hangoutLink']
+        elif 'conferenceData' in event:
+            location = event['conferenceData']['entryPoints'][0]['uri']
+        else:
+            location = ""
+
+        return cls(
+            start=start_date,
+            end=end_date,
+            title=event['summary'],
+            location=location,
+            gcal_id=event['id']
+        )
