9ef2261921169ef29a5e1ea25aae3cf804c7b46e
Santiago <shpotes3@gmail.com>
2022-02-26 21:10:10 -0500

    fix: now it doesn't update the old events

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import argparse
 from pathlib import Path
-import json
 
 from src import gcal, notion
+
+
+def filter_old_events(
+    notion_service,
+    database_id,
+    events
+):
+    notion_events = notion.get_events(notion_service, database_id)
+    notion_ids = {event.gcal_id for event in notion_events}
+    return [event for event in events if event.gcal_id not in notion_ids]
 
 
 def sync_events(
@@ -11,7 +20,8 @@ def sync_events(
     notion_service,
 ):
     events = gcal.get_events(gcal_service)
-    for event in events:
+    new_events = filter_old_events(notion_service, database_id, events)
+    for event in new_events:
         notion.create_event(
             notion_service,
             database_id,
@@ -19,10 +29,7 @@ def sync_events(
         )
 
 
-def main(secret_dir: Path):
-    with open(secret_dir / 'notion_secrets.json', 'r') as buf:
-        database_id = json.load(buf)['database_id']
-
+def main(secret_dir: Path, database_id: str):
     gcal_service = gcal.get_service(secret_dir)
     notion_service = notion.get_service(secret_dir)
 
@@ -32,5 +39,7 @@ def main(secret_dir: Path):
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument('--secret_dir', type=str, required=True)
+    args.add_argument('--database_id', type=str, required=True)
 
-    main(Path(args.parse_args().secret_dir))
+    parse_args = args.parse_args()
+    main(Path(parse_args.secret_dir), parse_args.database_id)
